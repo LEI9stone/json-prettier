65ce37db1295cc3ca4c344ac94b536c0b809d560
十号 <605002617@qq.com>
2023-12-06 09:09:39 +0800

    feat: diff viewer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,19 @@
-import { Input, Form, Button, Row, Col, Drawer } from "antd";
-import { useCallback, useState } from "react";
+import {
+	Input,
+	Form,
+	Button,
+	Row,
+	Col,
+	Drawer,
+	notification,
+	Alert,
+	Typography,
+	message,
+} from "antd";
+import { useCallback, useEffect, useRef, useState } from "react";
 import * as jsondiff from "jsondiffpatch";
+import "jsondiffpatch/dist/formatters-styles/html.css";
 import "./App.scss";
-import { isArray, isObject } from "lodash";
 
 const { TextArea } = Input;
 
@@ -13,7 +24,7 @@ interface FormStore {
 
 interface ResultStore {
 	open: boolean;
-	data: Record<string, unknown>;
+	html: string;
 }
 
 const beforeJSON = `{
@@ -322,216 +333,140 @@ const afterJSON = `{
   "code": 0
 }`;
 
-const DATA = {
-	data: {
-		banners: {
-			"1": [
-				{
-					imageUrl:
-						"//q01.yinge.cc/resource/2023/11/11/0efb93ea-dd1d-476d-9da4-e57625cd4cb5.jpg",
-				},
-			],
-			"2": [
-				{
-					imageUrl:
-						"//q01.yinge.cc/resource/2023/11/29/31c094e0-ef18-4055-81f2-791af53cf85f.jpg",
-				},
-			],
-			_t: "a",
-		},
-		productId: [10076, 10802],
-		productName: [
-			"淘麦VIP纪念票·亚克力实体票根",
-			"淘麦VIP纪念票实体演出票票根",
-		],
-		skus: {
-			"0": {
-				skuId: [890524, 897514],
-				productId: [10076, 10802],
-				skuName: ["6x12cm-透明款", "演出票亚克力票根"],
-				imageUrl: [
-					"@@ -29,47 +29,47 @@\n /10/\n-13/1a5f8efc-cccd-419a-abc5-3f40b2478fc8\n+25/7dd8923e-eab8-4fb9-8885-f242df769be0\n .jpg\n",
-					0,
-					2,
-				],
-				attrValues: {
-					"0": {
-						attrId: ["25604", "25752"],
-						valId: ["41784", "45164"],
-						valName: ["6x12cm", "演出票亚克力票根"],
-					},
-					_t: "a",
-					_1: [
-						{
-							attrId: "25615",
-							valId: "42082",
-							valName: "透明款",
-						},
-						0,
-						0,
-					],
-				},
-			},
-			"1": [
-				{
-					skuId: 897516,
-					productId: 10802,
-					skuName: "演出票纸质票根",
-					imageUrl:
-						"//q01.yinge.cc/resource/2023/10/25/95c5605f-8143-44fa-af2c-816fc5f2caaf.jpg",
-					unitPrice: 1990,
-					originalPrice: 1990,
-					labelValues: null,
-					attrValues: [
-						{
-							attrId: "25752",
-							valId: "45166",
-							valName: "演出票纸质票根",
-						},
-					],
-					config: {
-						useCollectionPage: false,
-						cutTypes: null,
-						subType: "",
-					},
-				},
-			],
-			_t: "a",
-		},
-		detailImages: {
-			"3": [
-				"//q01.yinge.cc/resource/2023/10/25/c7dda4ab-68f8-4a06-bb6c-1c5f5a86b5a8.jpg",
-			],
-			"4": [
-				"//q01.yinge.cc/resource/2023/10/25/e0e13677-2d6e-4609-8cdb-89f21b82e5f5.jpg",
-			],
-			"5": [
-				"//q01.yinge.cc/resource/2023/10/25/9babca82-4326-4d40-a9a4-5de332be127b.jpg",
-			],
-			"6": [
-				"//q01.yinge.cc/resource/2023/11/29/e0c1e10d-f9ec-4c03-9b63-820cbc41bf57.jpg",
-			],
-			"7": [
-				"//q01.yinge.cc/resource/2023/11/29/90efe44c-de00-4676-9204-3f0f6a76fdbc.jpg",
-			],
-			"8": [
-				"//q01.yinge.cc/resource/2023/11/29/29d51313-b285-46d3-ba83-fa0816fe42dd.jpg",
-			],
-			_t: "a",
-			_2: [
-				"//q01.yinge.cc/resource/2023/09/30/13d8ba3d-a529-444c-a19d-7b217a9741b4.png",
-				0,
-				0,
-			],
-			_4: [
-				"//q01.yinge.cc/resource/2023/09/30/82246b82-506b-430a-8a11-cd8be660f296.png",
-				0,
-				0,
-			],
-		},
-		shelfId: [91140, 91256],
-		attrList: {
-			"0": {
-				attrId: ["25604", "25613"],
-			},
-			"1": {
-				attrId: 
-        ["25615", "25604"],
-				attrName: ["亚克力", "尺寸"],
-			},
-			"2": {
-				attrId: ["24100", "25615"],
-				attrName: ["板式", "亚克力"],
-			},
-			"3": [
-				{
-					attrId: "24100",
-					attrName: "板式",
-					type: "attr",
-				},
-			],
-			_t: "a",
-		},
-		customType: ["430", "654"],
-	},
-	code: [1, 0],
-};
-
-function JsonViewer({ data }: { data: Record<string, unknown> }) {
-	if (isArray(data)) {
-		return <div className="pl-5px">
-      <div className="pl-5px">[</div>
-      <div className="pl-5px">{data.map((item) => <JsonViewer data={item} />)}</div>
-      <div className="pl-5px">]</div>
-    </div>;
-	}
-	if (isObject(data)) {
-		return <div className="pl-5px">
-      <div className="pl-5px">{`{`}</div>
-      <div className="pl-5px">{Object.keys(data).map((key) => <>
-      <div className="pl-5px">{key}:</div>
-      <div className="pl-5px"><JsonViewer data={data[key] as Record<string, unknown>} /></div>
-      </>)}</div>
-      <div className="pl-5px">{`}`}</div>
-    </div>;
-	}
-	return <span className="pl-5px">{data}</span>;
-}
-
 function App() {
 	const [form] = Form.useForm<FormStore>();
+	const keyComposeRef = useRef<string>();
 	const [result, setResult] = useState<Partial<ResultStore>>({
-		data: DATA,
-		open: true,
+		open: false,
 	});
 	const onFinished = useCallback(async () => {
-		const values = await form.validateFields().catch(() => null);
+		const values = await form
+			.validateFields()
+			.catch((errInfo: { errorFields: { errors: string[] }[] }) => {
+				const errMessages = errInfo.errorFields.map((field) => field.errors[0]);
+				notification.error({
+					message: "数据录入提示：",
+					description: errMessages.map((msg) => <div key={msg}>{msg}</div>),
+				});
+				return null;
+			});
 		if (!values) return;
-		const before = JSON.parse(values.before);
-		const after = JSON.parse(values.after);
-		const delta = jsondiff.diff(before, after);
-		if (!delta) return;
-		setResult({ open: true, data: delta });
-		console.log("delta", delta);
+		try {
+			const before = JSON.parse(values.before);
+			const after = JSON.parse(values.after);
+			const delta = jsondiff.diff(before, after);
+      //  正则验证，是否为{、[ 开头或者 }、]结尾
+			if (!delta) {
+				notification.success({
+					message: "数据对比操作：",
+					description: "两份数据的对比结果一致",
+				});
+				return;
+			}
+			const htmlSrt = jsondiff.formatters.html.format(delta, before);
+			jsondiff.formatters.html.hideUnchanged();
+			setResult({ open: true, html: htmlSrt });
+		} catch (error) {
+			if (!error) {
+				message.warning("未知错误");
+				return;
+			}
+      const err = error as Error;
+			notification.error({
+				message: "数据对比操作：",
+				description: err?.message
+			});
+		}
 	}, [form]);
+	useEffect(() => {
+		const keydownHandle = (ev: KeyboardEvent) => {
+			if (!ev || !ev.key) {
+				keyComposeRef.current = void 0;
+				return;
+			}
+			const key = ev.key.toLocaleLowerCase();
+			if (!["control", "enter"].includes(key)) {
+				return;
+			}
+			keyComposeRef.current = `${keyComposeRef.current || ""}${key}`;
+			if (["controlenter", "entercontrol"].includes(keyComposeRef.current!)) {
+				keyComposeRef.current = void 0;
+				onFinished();
+			}
+		};
+		const keyupHandle = () => {
+			keyComposeRef.current = void 0;
+		};
+		window.addEventListener("keydown", keydownHandle);
+		window.addEventListener("keyup", keyupHandle);
+		return () => {
+			window.removeEventListener("keydown", keydownHandle);
+			window.removeEventListener("keyup", keyupHandle);
+		};
+	}, [onFinished]);
 	return (
 		<div>
-			<Form
-				form={form}
-				layout='vertical'
-				initialValues={{ before: beforeJSON, after: afterJSON }}
-			>
-				<Row gutter={[16, 16]}>
-					<Col span={12} className='relative'>
-						<Form.Item
-							name='before'
-							required
-							className='flex-1'
-							label='原始数据'
-							rules={[{ required: true, message: "请输入原始数据" }]}
-						>
-							<TextArea className='h-96vh!' />
-						</Form.Item>
-						<Button type='primary' className='control' onClick={onFinished}>
-							对比
-						</Button>
-					</Col>
-					<Col span={12}>
-						<Form.Item
-							name='after'
-							required
-							className='flex-1'
-							label='对比数据'
-							rules={[{ required: true, message: "请输入对比数据" }]}
-						>
-							<TextArea className='h-96vh!' />
-						</Form.Item>
-					</Col>
-				</Row>
+			<Form form={form} layout='vertical'>
+				<Form.Item
+					extra={
+						<Typography>
+							<Typography.Title level={3}>JSON-DIFF 说明：</Typography.Title>
+							<Typography.Paragraph>
+								<ul>
+									<li>请确保原JSON数据和对比的JSON数据格式正确</li>
+									<li>
+										<Typography.Text keyboard>Control + Enter</Typography.Text>
+										键可以唤起对比结果
+									</li>
+								</ul>
+							</Typography.Paragraph>
+						</Typography>
+					}
+				>
+					<Row gutter={[16, 16]}>
+						<Col span={12} className='relative'>
+							<Form.Item
+								name='before'
+								required
+								className='flex-1'
+								label='原始数据'
+								rules={[{ required: true, message: "请输入原始数据" }]}
+							>
+								<TextArea
+									className='h-70vh!'
+									placeholder="JSON 格式的数据，例如：{a: 1, b: 'test'}"
+								/>
+							</Form.Item>
+							<Button type='primary' className='control' onClick={onFinished}>
+								对比
+							</Button>
+						</Col>
+						<Col span={12}>
+							<Form.Item
+								name='after'
+								required
+								className='flex-1'
+								label='对比数据'
+								rules={[{ required: true, message: "请输入对比数据" }]}
+							>
+								<TextArea
+									className='h-70vh!'
+									placeholder='输入需要对比的JSON数据'
+								/>
+							</Form.Item>
+						</Col>
+					</Row>
+				</Form.Item>
 			</Form>
-			<Drawer open={result.open} width={1200} title='数据对比结果'>
-        <JsonViewer data={result.data} />
-      </Drawer>
+			<Drawer
+				open={result.open}
+				width={1200}
+				title='数据对比结果'
+				maskClosable={false}
+				onClose={() => setResult({})}
+			>
+				<div dangerouslySetInnerHTML={{ __html: result.html! }}></div>
+			</Drawer>
 		</div>
 	);
 }
